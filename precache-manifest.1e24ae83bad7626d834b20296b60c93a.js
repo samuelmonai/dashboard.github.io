02905e757aa7c0ef483cf216645ad22e7b77c9ec
samuel monai <samuelmonai@hotmail.com>
2018-10-15 21:44:43 -0300

    primeiro commit

---
+++ precache-manifest.1e24ae83bad7626d834b20296b60c93a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3cff265142b336640041",
+    "url": "/static/js/main.3cff2651.chunk.js"
+  },
+  {
+    "revision": "50f6070d577bf4df9881",
+    "url": "/static/js/1.50f6070d.chunk.js"
+  },
+  {
+    "revision": "3cff265142b336640041",
+    "url": "/static/css/main.0ff35fd4.chunk.css"
+  },
+  {
+    "revision": "50f6070d577bf4df9881",
+    "url": "/static/css/1.0c0ed84f.chunk.css"
+  },
+  {
+    "revision": "7d7daf73edca9e5a07d3cf99e2faa94c",
+    "url": "/index.html"
+  }
+];
